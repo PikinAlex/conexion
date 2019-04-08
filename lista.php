25f8a37bbe89ac47427ff9060b0885c87334126f
PikinAlex <49386571+PikinAlex@users.noreply.github.com>
2019-04-08 02:55:27 -0600

    Update lista.php

--- lista.php
+++ lista.php
@@ -1,5 +1,15 @@
 <?php
-include 'dbconfig.php';
+//Define your host here.
+$HostName = "localhost";
+ 
+//Define your database name here.
+$DatabaseName = "catalago_peliculas";
+ 
+//Define your database username here.
+$HostUser = "root";
+ 
+//Define your database password here.
+$HostPass = "";
  
 // Create connection
 $conn = new mysqli($HostName, $HostUser, $HostPass, $DatabaseName);
